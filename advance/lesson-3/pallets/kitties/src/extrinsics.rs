67d7c5386b60239c389631efe279bdbb15c3d4df
papermoonzhou <zhou@papermoon.io>
2024-08-05 21:10:42 +0800

    update

--- advance/lesson-3/pallets/kitties/src/extrinsics.rs
+++ advance/lesson-3/pallets/kitties/src/extrinsics.rs
@@ -3,28 +3,21 @@ use frame_support::pallet_macros::pallet_section;
 /// Define all extrinsics for the pallet.
 #[pallet_section]
 mod dispatches {
-    /// Dispatchable functions allow users to interact with the pallet and invoke state changes.
-    /// These functions materialize as "extrinsics", which are often compared to transactions.
-    /// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        pub fn create(origin: OriginFor<T>, something: u32) -> DispatchResult {
+        pub fn create(origin: OriginFor<T>) -> DispatchResult {
             let who = ensure_signed(origin)?;
             let _value = Self::random_value(&who);
-
-            // Something::<T>::put(something);
-
-            // Self::deposit_event(Event::SomethingStored { something, who });
 
             Ok(())
         }
 
-        pub fn breed(origin: OriginFor<T>) -> DispatchResult {
+        pub fn breed(origin: OriginFor<T>, kitty_1: u32, kitty_2: u32) -> DispatchResult {
             let _who = ensure_signed(origin)?;
             Ok(())
         }
 
-        pub fn transfer(origin: OriginFor<T>) -> DispatchResult {
+        pub fn transfer(origin: OriginFor<T>, kitty_id: u32) -> DispatchResult {
             let _who = ensure_signed(origin)?;
             Ok(())
         }
